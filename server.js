39f42c3aa7ef077827c93cf6c3637abb139dae3d
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-24 11:22:49 +1100

    udpate

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var bodyParser = require('body-parser');
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+
+var dbUrl = "mongodb+srv://admin:admin@chatapp-db.hhq8z.mongodb.net/admin?retryWrites=true&w=majority";
+
+var Message = mongoose.model('Message',{
+    name: String,
+    message: String
+})
+
 var messages = [
     {name: 'Tim', message: 'Hello'},
     {name: 'Jake', message: 'Hi'}
@@ -18,14 +29,25 @@ app.get('/message', (req, res) => {
 })
 
 app.post('/message', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    let message = new Message(req.body)
+    message.save((err) => {
+        if (err) {
+            sendStatus(500)
+        }
+        messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+
     
 })
 
 io.on('connection', (socket) => {
     console.log('connected')
+})
+
+mongoose.connect(dbUrl,   (err) => {
+    console.log('mongo db connection',err)
 })
 
 let server = http.listen(3000, () => {
